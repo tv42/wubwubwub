8b353fcb571a128af93232fce4c2e99efae079e9
moshez <moshez@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2002-11-13 10:43:15 +0000

    Commiting version 12
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@2 e8c61fa8-82ed-0310-a1ef-921661bf76ae

---
+++ 00addhosts.py
@@ -0,0 +1,12 @@
+# Add all hosts in a given directory as virtual hosts.
+# In order to use this, change '/var/www/vhost' to the directory
+# in which you keep the files for the virtual hosts, and copy this file to
+# /etc/twisted-web/local.d
+#
+from twisted.web import static
+import os
+
+vhostDir = '/var/www/vhost/'
+
+for file in os.listdir(vhostDir):
+    root.addHost(file, static.File(os.path.join(vhostDir, file)))
---
+++ debian/init.d
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+pidfile=/var/run/twisted-web.pid
+rundir=/var/lib/twisted-web/
+file=/etc/twisted-web/twisted-web
+logfile=/var/log/twisted-web.log
+more_args=--no_save
+
+[ -r /etc/default/twisted-web ] && . /etc/default/twisted-web
+
+test -x /usr/bin/twistd2.2 || exit 0
+test -r $file || exit 0
+test -r /usr/share/twisted-web/package-installed || exit 0
+test -r /etc/twisted-web/disable && . /etc/twisted-web/disable
+
+
+case "$1" in
+    start)
+        echo -n "Starting twisted-web: twistd"
+        start-stop-daemon --start --quiet --exec /usr/bin/twistd2.2 --  \
+                          --pidfile=$pidfile --rundir=$rundir --python=$file\
+                          --logfile=$logfile --quiet $more_args
+        echo "."	
+    ;;
+
+    stop)
+        echo -n "Stopping twisted-web: twistd"
+        start-stop-daemon --stop --quiet --pidfile $pidfile
+        echo "."	
+    ;;
+
+    restart)
+        $0 stop
+        $0 start
+    ;;
+
+    force-reload)
+        $0 restart
+    ;;
+
+    *)
+        echo "Usage: /etc/init.d/twisted-web {start|stop|restart|force-reload}" >&2
+        exit 1
+    ;;
+esac
+
+exit 0
