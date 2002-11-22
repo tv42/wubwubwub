970e8383239f6d8c65aac7fe6977a519d762060c
moshez <moshez@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2002-11-22 11:04:39 +0000

    Add another example, of adding a specially configured vhost
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@49 e8c61fa8-82ed-0310-a1ef-921661bf76ae

---
+++ examples/05addexample.py
@@ -0,0 +1,47 @@
+# In order to add a custom virtual host, copy this file into
+# /etc/twisted-web/local.d/05add<host name>.py, change
+# "example.com" and "/var/www/example" to the hostname and
+# directory you want, and change the various settings as per
+# the comments.
+#
+from twisted.web import script, static, twcgi
+
+vhostName, vhostRoot= "example.com", "/var/www/example"
+resource = static.File('/var/www/'+vhostRoot)
+root.addHost(vhostName, vhostResource)
+
+# The default processors in Twisted, by file extension.
+# These make some file extensions not being sent to the user,
+# but rather evaluated in various ways.
+#
+# Do *NOT* turn any of this on if untrusted users can put files
+# in the document hierarchy.
+resource.processors = {
+#    '.cgi': twcgi.CGIScript,
+#    '.php3': twcgi.PHP3Script,
+#    '.php': twcgi.PHPScript,
+#    '.epy': script.PythonScript,
+#    '.rpy': script.ResourceScript,
+#    '.trp': trp.ResourceUnpickler,
+}
+
+# Change this if you want default mime-type to be different:
+# resource.defaultType = 'text/html'
+
+# Add more mime-types
+resource.contentTypes = root.contentTypes.copy()
+# resource.contentTypes['extension'] = 'mime/type'
+
+# Uncomment this so /path/to/foo can serve /path/to/foo.bar
+# resource.allowExt = 1
+
+# Add more indexes
+# http://host/foo/ will serve index.html, index.rpy or index.php,
+# the first which exists.
+# If none exist, it will generate a directory listing.
+#
+resource.indexNames = [
+    'index.html',
+#     'index.rpy',
+#     'index.php',
+]
