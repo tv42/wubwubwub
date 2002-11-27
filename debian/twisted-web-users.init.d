aa698601ce0143aaac48de821810d74196954b22
moshez <moshez@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2002-11-27 16:32:48 +0000

    .allow and .deny support
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@65 e8c61fa8-82ed-0310-a1ef-921661bf76ae

--- debian/twisted-web-users.init.d
+++ debian/twisted-web-users.init.d
@@ -7,7 +7,15 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 test -x /usr/bin/twistd2.2 || exit 0
 test -r /usr/share/twisted-web/package-installed || exit 0
 
-users=`getent passwd|awk -F: '$3>=1000 && $1!="nobody" {printf "grep -q %s /etc/shells && echo %s\n",$7,$1}'|sh`
+if [ -r /etc/twisted-web/twisted-web-users.allow ]; then
+    users=`cat /etc/twisted-web/twisted-web-users.allow`
+else
+    users=`getent passwd|awk -F: '$3>=1000 && $1!="nobody" {printf "grep -q %s /etc/shells && echo %s\n",$7,$1}'|sh`
+    if [ -r /etc/twisted-web/twisted-web-users.deny ]; then
+        temp_users=`for user in $users;do echo $user;done | sort | comm -2 -3 - <(sort /etc/twisted-web-users.deny)`
+        users=$temp_users
+    fi
+fi
 
 case "$1" in
     start)
