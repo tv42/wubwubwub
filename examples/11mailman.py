b06581dd62047167f82ab8fe4d2abbb0a23bb378
moshez <moshez@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2003-05-29 09:32:56 +0000

    If there's to be a mailman, there has to be a pipermail
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@135 e8c61fa8-82ed-0310-a1ef-921661bf76ae

--- examples/11mailman.py
+++ examples/11mailman.py
@@ -1,3 +1,6 @@
 # Make sure mailman links work
-from twisted.web import twcgi
+from twisted.web import twcgi, static
 default.putChild('mailman', twcgi.CGIDirectory('/usr/lib/cgi-bin'))
+
+# Comment this out if you do not want to use the pipermail archiver
+default.putChild('pipermail', static.File('/var/lib/mailman/archives/public'))
