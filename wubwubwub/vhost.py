4844c725d80b04de4f284090eaebe592bf7146b4
tv <tv@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2005-02-16 12:20:28 +0000

    Protect against self.logFile=None
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@166 e8c61fa8-82ed-0310-a1ef-921661bf76ae

--- wubwubwub/vhost.py
+++ wubwubwub/vhost.py
@@ -7,15 +7,17 @@ class VhostLoggingNevowSite(appserver.NevowSite):
         Log a request's result to the logfile.
         Use combined log format prefixed with virtual host name.
         """
-        if hasattr(self, "logFile"):
-            line = '%r %s - - %s "%s" %d %s "%s" "%s"\n' % (
-                request.getHeader('host'),
-                request.getClientIP(),
-                # request.getUser() or "-", # the remote user is almost never important
-                http._logDateTime,
-                '%s %s %s' % (request.method, request.uri, request.clientproto),
-                request.code,
-                request.sentLength or "-",
-                request.getHeader("referer") or "-",
-                request.getHeader("user-agent") or "-")
-            self.logFile.write(line)
+        logFile = getattr(self, 'logFile', None)
+        if logFile is None:
+            return
+        line = '%r %s - - %s "%s" %d %s "%s" "%s"\n' % (
+            request.getHeader('host'),
+            request.getClientIP(),
+            # request.getUser() or "-", # the remote user is almost never important
+            http._logDateTime,
+            '%s %s %s' % (request.method, request.uri, request.clientproto),
+            request.code,
+            request.sentLength or "-",
+            request.getHeader("referer") or "-",
+            request.getHeader("user-agent") or "-")
+        self.logFile.write(line)
