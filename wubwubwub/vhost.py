27ca062a236ffec80121723c860680384187df4f
tv <tv@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2005-02-16 12:13:57 +0000

    Move VhostLoggingNevowSite to wubwubwub.vhost.
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@164 e8c61fa8-82ed-0310-a1ef-921661bf76ae

---
+++ wubwubwub/vhost.py
@@ -0,0 +1,18 @@
+from twisted.protocols import http
+from nevow import appserver
+
+class VhostLoggingNevowSite(appserver.NevowSite):
+    def log(self, request):
+        """Log a request's result to the logfile, in combined log format prefixed with virtual host name."""
+        if hasattr(self, "logFile"):
+            line = '%r %s - - %s "%s" %d %s "%s" "%s"\n' % (
+                request.getHeader('host'),
+                request.getClientIP(),
+                # request.getUser() or "-", # the remote user is almost never important
+                http._logDateTime,
+                '%s %s %s' % (request.method, request.uri, request.clientproto),
+                request.code,
+                request.sentLength or "-",
+                request.getHeader("referer") or "-",
+                request.getHeader("user-agent") or "-")
+            self.logFile.write(line)
