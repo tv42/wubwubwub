81fec3cd9b08671ddebb5c4ee3ef6f77cce08cc0
tv <tv@e8c61fa8-82ed-0310-a1ef-921661bf76ae>
2005-02-16 12:53:31 +0000

    Move site config to wubwubwub.config.
    git-svn-id: svn+ssh://open.inoi.fi/open-wubwubwub/trunk@173 e8c61fa8-82ed-0310-a1ef-921661bf76ae

--- wubwubwub/config.py
+++ wubwubwub/config.py
@@ -1,5 +1,6 @@
 """WubWubWub configuration defaults."""
 from twisted.web import static
+from wubwubwub import vhost
 
 # Set user/group to run under
 # Usually, it runs under the www-data user and group.
@@ -12,3 +13,14 @@ default = static.File('/var/www')
 # Make sure it is easy to add new virtual hosts:
 root = vhost.NameVirtualHost()
 root.default = default
+
+# This is the *Apache compatible* log file, not the twisted-style logfile.
+# Leaving this as None will have no Apache compatible log file. Apache
+# compatible logfiles are useful because there are quite a few programs
+# which analyse them and display statistics. 
+#logPath = None
+logPath = '/var/log/wubwubwub/access.log'
+
+# Generate the Site factory. You will not normally
+# want to modify this line.
+site = vhost.VhostLoggingNevowSite(root, logPath=logPath)
